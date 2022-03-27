780ec27e403bf23829d38066ba52606f08a03f17
Jeyoung-Park <jason9380@naver.com>
2022-03-28 01:22:45 +0900

    fix: movie detail page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import NavBar from "../components/NavBar";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `movies/${id}`
+    );
+  };
+
   return (
     <div>
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div key={movie.id}>
-          <h4>{movie.original_title}</h4>
+        <div
+          key={movie.id}
+          onClick={() => onClick(movie.id, movie.original_title)}
+        >
+          <div>
+            <a>{movie.original_title}</a>
+          </div>
         </div>
       ))}
     </div>
