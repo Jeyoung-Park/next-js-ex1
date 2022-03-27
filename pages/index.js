985cea3cea120929d5583cd29076e3e333d548dc
Jeyoung-Park <jason9380@naver.com>
2022-03-28 01:02:30 +0900

    feat: serverside rendering

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,26 @@ import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch("/api/movies")).json();
-      setMovies(results);
-    })();
-  }, []);
-
+export default function Home({ results }) {
   return (
     <div>
       <Seo title="Home" />
-      {!movies && <h4>Loading....</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div key={movie.id}>
           <h4>{movie.original_title}</h4>
         </div>
       ))}
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
