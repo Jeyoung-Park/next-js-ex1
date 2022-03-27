80c631d1c7da83f8e0795e24cc21382e994d4592
Jeyoung-Park <jason9380@naver.com>
2022-03-28 00:26:31 +0900

    feat: movie api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
+
+const API_KEY = "80c11b23ceace0b1fd6777287bd954d1";
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <Seo title="Home" />
+      {!movies && <h4>Loading....</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
